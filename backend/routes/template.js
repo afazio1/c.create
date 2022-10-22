21f2b79028b907bf2900d41291ada3372a5d8c2f
afazio1 <alexa.fazio04@gmail.com>
2022-10-22 02:49:02 -0400

    setup route boilerplate

---
+++ backend/routes/template.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/")
+    .post((req, res) => {
+
+    })
+    .get((req, res) => {
+
+    })
+
+router.route("/:id")
+    .post((req, res) => {
+
+    })
+    .get((req, res) => {
+
+    })
+    .delete((req, res) => {
+
+    })
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,26 @@ const app = express();
 
 const connectDB = require("./config/db");
 const mongoose = require("mongoose");
-
-
 connectDB();
+
+require("dotenv").config();
+
+const templateRoutes = require("./routes/template");
+const triggerRoutes = require("./routes/trigger");
+const actionRoutes = require("./routes/action");
+const workflowRoutes = require("./routes/workflow");
+
+app.use("/templates", templateRoutes);
+app.use("/triggers", triggerRoutes);
+app.use("/actions", actionRoutes);
+app.use("/workflows", workflowRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+});
